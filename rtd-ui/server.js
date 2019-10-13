3c5ae10adc992399d232ec3c526926e2837b8007
Derek Shao <shao.derek.now@gmail.com>
2019-10-13 17:07:18 -0400

    React Server

---
+++ rtd-ui/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.info("Server listening on port %s...", PORT);
+});
